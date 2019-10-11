dd93849e95f72641324ce4a40dc15922adb4fdc1
Mikel M <mikelmayorga08@gmail.com>
2019-10-11 15:27:37 -0700

    First commit

---
+++ TestCreator/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestCreator.Models;
+
+[assembly: HostingStartup(typeof(TestCreator.Areas.Identity.IdentityHostingStartup))]
+namespace TestCreator.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TestCreatorContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TestCreatorContextConnection")));
+
+                //services.AddDefaultIdentity<IdentityUser>()
+                //    .AddEntityFrameworkStores<TestCreatorContext>();
+            });
+        }
+    }
+}
---
+++ TestCreatorDatabase/table_script.sql
@@ -0,0 +1,40 @@
+﻿create table [dbo].[instructor]
+(
+	[instructorID] INT NOT NULL PRIMARY KEY IDENTITY(10000, 111),
+	[fname] NVARCHAR(50) NOT NULL,
+	[lname] NVARCHAR(50) NOT NULL,
+)
+create table [dbo].[class]
+(
+	[classID] INT NOT NULL PRIMARY KEY IDENTITY(1000, 101),
+	[class_name] NVARCHAR(50) NOT NULL,
+	[class_subject] NVARCHAR(50) NOT NULL
+)
+create table [dbo].[student]
+(
+	[studentID] INT NOT NULL PRIMARY KEY IDENTITY(20000, 11),
+	[f_name] NVARCHAR(50) NOT NULL,
+	[l_name] NVARCHAR(50) NOT NULL
+)
+create table [dbo].[test]
+(
+	[testID] INT NOT NULL PRIMARY KEY IDENTITY(100, 11),
+	[test_title] NVARCHAR(100) NOT NULL,
+	[num_questions] INT NOT NULL,
+)
+create table [dbo].[question]
+(
+	[questionID] INT NOT NULL PRIMARY KEY IDENTITY(10000, 5),
+	[question_content] NVARCHAR(MAX) NOT NULL,
+)
+create table [dbo].[answer]
+(
+	[answerID] INT NOT NULL PRIMARY KEY IDENTITY(10000, 1),
+	[answer_content] NVARCHAR(100) NOT NULL
+)
+create table [dbo].[test_score]
+(
+	[scoreID] INT NOT NULL PRIMARY KEY IDENTITY(100000, 111),
+	[score_percentage] DECIMAL,
+	[number_correct] INT
+)
