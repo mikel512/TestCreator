039849db9c23281a4b1b91e51d28f754c347d69f
Mikel M <mikelmayorga08@gmail.com>
2019-10-19 17:03:56 -0700

    Added SqlDataAccess class

---
+++ DataAccess/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.DataAccess
+{
+    class SqlDataAccess
+    {
+        public string GetConnectionString()
+        {
+            var conn = new AppConfiguration();
+            return conn.ConnectionString;
+        }
+    }
+}
