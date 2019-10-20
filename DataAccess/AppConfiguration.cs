623bcff9f4361db8f3197be71af621e0dd4d4ff0
Mikel M <mikelmayorga08@gmail.com>
2019-10-19 17:06:58 -0700

    Added rest of updated files

---
+++ DataAccess/AppConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DataLibrary
+{
+    public class AppConfiguration
+    {
+        public readonly string _connectionString = string.Empty;
+        public AppConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configurationBuilder.AddJsonFile(path, false);
+
+            var root = configurationBuilder.Build();
+            _connectionString = root.GetSection("ConnectionStrings").GetSection("DefaultConnection").Value;
+            var appSetting = root.GetSection("ApplicationSettings");
+        }
+        public string ConnectionString
+        {
+            get => _connectionString;
+        }
+    }
+}
