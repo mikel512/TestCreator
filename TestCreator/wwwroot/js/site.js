5abae722cc6078a3cd37dcae4949b21ad2157d14
Mikel M <mikelmayorga08@gmail.com>
2019-11-15 18:35:46 -0800

    Registration and Login working

--- TestCreator/wwwroot/js/site.js
+++ TestCreator/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Login validation
+$("#loginButton").click(function (e) {
+    if ($("#emailTextBox").val() == "") {
+        $("#emailSpan").text("Enter Email");
+    }
+    else {
+        $("#emailSpan").text("");
+    }
+    if ($("#passwordTextBox").val() == "") {
+        $("#passwordSpan").text("Enter Password");
+    }
+    else {
+        $("#passwordSpan").text("");
+    }
+
+    var data = {};
+    data.Email = $("#emailTextBox").val();
+    data.Password = $(#)
+    $.ajax({
+        type:   "POST",
+        url:    
+    })
+})
