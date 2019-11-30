1a66d2271a9c6613aaed358fcdbfb1d121e4f7cc
Mikel M <mikelmayorga08@gmail.com>
2019-11-29 23:05:51 -0800

    Added ExamCreation link to _layout

--- TestCreator/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ TestCreator/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -7,16 +7,17 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TestCreator.Areas.Identity.Data;
 
 namespace TestCreator.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<TestUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<TestUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
